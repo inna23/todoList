8a02ca27bc6892aef902533b22fd35a79e3548f0
Inna Oreshkevich <inna.oreshkevich@sitrus.com>
2018-03-09 21:20:08 -0800

    Initial commit

---
+++ src/layout/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todoList: [],
+      todoInput: 'abc',
+    };
+  }
+
+  addTodo() {
+    const todoList = this.state.todoList;
+    todoList.push(this.state.todoInput);
+
+    this.setState({
+      todoList,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.todoInput}
+          onChange={e => this.setState({ todoInput: e.target.value })}
+        />
+        <button onClick={() => this.addTodo()}>Add</button>
+        <ul>
+          {
+            this.state.todoList.map((el, i) => <li key={i}>{el}</li>)
+          }
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
