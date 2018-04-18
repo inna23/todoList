0745c1c1818cb4a4e47a95331d7df1fe6b30387f
Inna Oreshkevich <inna.oreshkevich@sitrus.com>
2018-04-18 15:30:32 -0700

    Add calculator conected to server 'https://tranquil-spire-74602.herokuapp.com/calculator/sub'

---
+++ src/calculator/calculator.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      a: 0,
+      b: 0,
+      sum: 0,
+      sub: 0
+    };
+  }
+
+  sum() {
+    fetch('https://tranquil-spire-74602.herokuapp.com/calculator/sum', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ a: this.state.a, b: this.state.b }),
+    })
+      .then(result => result.json())
+      .then((result) => {
+        this.setState({ sum: result });
+        console.log(result);
+      });
+  }
+
+  sub() {
+    fetch('https://tranquil-spire-74602.herokuapp.com/calculator/sub', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ a: this.state.a, b: this.state.b }),
+    })
+      .then(result => result.json())
+      .then((result) => {
+        this.setState({ sub: result });
+        console.log(result);
+      });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <h2>Calculator</h2>
+          <input
+            className="form-control"
+            type="text"
+            onChange={(e) => { this.setState({ a: e.target.value }); }}
+          />
+          <input
+            className="form-control"
+            type="text"
+            onChange={(e) => { this.setState({ b: e.target.value }); }}
+          />
+          <button className="btn btn-outline-dark" onClick={() => this.sum()}> Sum </button>
+          <button className="btn btn-outline-dark" onClick={() => this.sub()}> Sub </button>
+        </div>
+        <h4>{this.state.sum}</h4>
+        <h4>{this.state.sub}</h4>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import App from './layout/App';
+// import App from './layout/App';
+import Calculator from './calculator/calculator';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Calculator />, document.getElementById('root'));
 registerServiceWorker();
